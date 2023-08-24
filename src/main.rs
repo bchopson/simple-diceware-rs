fedf69066cc5e8fb60964735be0e75f3f80ba06b
Ben Chopson <bchopson@users.noreply.github.com>
2023-08-23 21:02:38 -0400

    Thanks ChatGPT

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use rand::Rng;
+use std::env;
+
+fn get_list() -> std::collections::HashMap<String, String> {
+    let wordlist = include_str!("../assets/eff_large_wordlist.txt");
+    let mut diceware_list = std::collections::HashMap::new();
+    for line in wordlist.lines() {
+        let parts: Vec<&str> = line.split('\t').collect();
+        diceware_list.insert(parts[0].to_owned(), parts[1].to_owned());
+    }
+    diceware_list
+}
+
+fn get_phrase(diceware_list: &std::collections::HashMap<String, String>) -> String {
+    let mut rng = rand::thread_rng();
+    let mut key = String::new();
+    for _ in 0..5 {
+        key.push_str(&(rng.gen_range(1..=6)).to_string());
+    }
+    diceware_list.get(&key).unwrap().to_owned()
+}
+
+fn generate_password(length: usize) -> String {
+    if length < 1 {
+        panic!("Password must be at least one phrase");
+    }
+    let diceware_list = get_list();
+    (0..length)
+        .map(|_| get_phrase(&diceware_list))
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let length = if args.len() > 1 {
+        args[1].parse().expect("Invalid length argument")
+    } else {
+        6
+    };
+    println!("{}", generate_password(length));
+}
